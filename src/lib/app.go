aed77d63a03ba5fb61464a32f0fbf798ac99b07f
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-05-31 16:38:12 +0700

    feat (KVStore): KVStore functionalities

--- src/lib/app.go
+++ src/lib/app.go
@@ -1,9 +1,58 @@
 package lib
 
+import "sync"
+
 type KVStore struct {
+	mu    sync.RWMutex
 	store map[string]string
 }
 
 func NewKVStore() *KVStore {
 	return &KVStore{store: make(map[string]string)}
+}
+
+// Get return empty string if key not found
+func (k *KVStore) Get(key string) string {
+	k.mu.RLock()
+	defer k.mu.RUnlock()
+	value, ok := k.store[key]
+
+	if !ok {
+		return ""
+	}
+
+	return value
+}
+
+func (k *KVStore) Set(key string, value string) {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	k.store[key] = value
+}
+
+func (k *KVStore) Len(key string) int {
+	k.mu.RLock()
+	defer k.mu.RUnlock()
+	value, ok := k.store[key]
+	if !ok {
+		return 0
+	}
+	return len(value)
+}
+
+func (k *KVStore) Delete(key string) string {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	value, ok := k.store[key]
+	if !ok {
+		return ""
+	}
+	delete(k.store, key)
+	return value
+}
+
+func (k *KVStore) Append(key string, value string) {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	k.store[key] += value
 }
