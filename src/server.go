e041a2de38fd61d1836aa081d662e030dda0460e
Marthenn <89301265+Marthenn@users.noreply.github.com>
2024-05-31 14:49:14 +0700

    feat (server): command line arguments parsing

---
+++ src/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func startServing(addr net.Addr, contactAddr net.Addr) {
+	fmt.Println("Starting server at", addr.String())
+	fmt.Println("Contacting server at", contactAddr.String())
+
+	//TODO: implement the server logic
+	/* from guidebook
+	with SimpleXMLRPCServer((addr.ip, addr.port)) as server:
+	        server.register_introspection_functions()
+	        server.register_instance(RaftNode(KVStore(), addr, contact_node_addr))
+	        server.serve_forever()
+	*/
+}
+
+func main() {
+	// take command line arguments (ip and port)
+	if len(os.Args) != 5 {
+		fmt.Println("Usage: go run client.go <ip> <port> <contact_ip> <contact_port>")
+		os.Exit(0)
+	}
+
+	addr, err := net.ResolveTCPAddr("tcp", os.Args[1]+":"+os.Args[2])
+	if err != nil {
+		fmt.Println("Error resolving address")
+		os.Exit(0)
+	}
+
+	contactAddr, err := net.ResolveTCPAddr("tcp", os.Args[3]+":"+os.Args[4])
+	if err != nil {
+		fmt.Println("Error resolving contact address")
+		os.Exit(0)
+	}
+
+	startServing(addr, contactAddr)
+}
