3a001659629a7aa5033371bd0eedcfe0f057a34f
Nelsen Putra <88304550+nelsenputra@users.noreply.github.com>
2024-06-05 13:54:00 +0700

    feat: implement server logic in server.go

--- src/server.go
+++ src/server.go
@@ -3,23 +3,79 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/rpc"
 	"os"
+	"sync"
 )
+
+type KVStore struct {
+	store map[string]string
+	mu    sync.Mutex
+}
+
+func NewKVStore() *KVStore {
+	return &KVStore{store: make(map[string]string)}
+}
+
+func (kv *KVStore) Get(key string, value *string) error {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	if v, ok := kv.store[key]; ok {
+		*value = v
+		return nil
+	}
+	return fmt.Errorf("key not found")
+}
+
+func (kv *KVStore) Put(kvPair [2]string, ack *bool) error {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.store[kvPair[0]] = kvPair[1]
+	*ack = true
+	return nil
+}
+
+type RaftNode struct {
+	store        *KVStore
+	addr         net.Addr
+	contactAddr  net.Addr
+}
+
+func NewRaftNode(store *KVStore, addr net.Addr, contactAddr net.Addr) *RaftNode {
+	return &RaftNode{store: store, addr: addr, contactAddr: contactAddr}
+}
+
+// You can add RaftNode methods here to handle RPC calls.
 
 func startServing(addr net.Addr, contactAddr net.Addr) {
 	fmt.Println("Starting server at", addr.String())
 
 	if contactAddr != nil {
 		fmt.Println("Contacting server at", contactAddr.String())
+		// Here you can implement the logic to contact the other node if necessary
 	}
 
-	//TODO: implement the server logic
-	/* from guidebook
-	with SimpleXMLRPCServer((addr.ip, addr.port)) as server:
-	        server.register_introspection_functions()
-	        server.register_instance(RaftNode(KVStore(), addr, contact_node_addr))
-	        server.serve_forever()
-	*/
+	kvStore := NewKVStore()
+	raftNode := NewRaftNode(kvStore, addr, contactAddr)
+
+	rpc.Register(raftNode)
+	listener, err := net.Listen("tcp", addr.String())
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on", addr.String())
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go rpc.ServeConn(conn)
+	}
 }
 
 func main() {
